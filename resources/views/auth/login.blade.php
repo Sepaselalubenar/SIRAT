f78022b8bd7ab4c569804340304a76b65e8e091d
xcoook1es <zghifari7@gmail.com>
2026-07-15 14:03:03 +0700

    perbesar pesan error login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,9 +44,9 @@
 
         <!-- Errors Alert -->
         @if ($errors->any())
-            <div class="bg-red-50 text-red-600 text-xs rounded-xl p-3.5 mb-6 border border-red-200 shadow-sm shadow-red-500/5">
-                <div class="flex gap-2">
-                    <svg class="w-4 h-4 text-red-500 shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <div class="bg-red-50 text-red-600 text-sm rounded-xl p-3.5 mb-6 border border-red-200 shadow-sm shadow-red-500/5">
+                <div class="flex gap-2 items-start">
+                    <svg class="w-5 h-5 text-red-500 shrink-0 mt-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                     </svg>
                     <span>{{ $errors->first() }}</span>
